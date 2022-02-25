770d15254967b587a48ef44e1f285909c4a34bca
Elias <eliasventura23@gmail.com>
2022-02-25 17:42:49 -0500

    commit tree 504a18458406250f995871c651012bb67c2246e0

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet < 400) {
+    return "This one is on me!";
+  }
+  else if (feet >= 2500) {
+    return "No can do.";
+  }
+  else if (feet > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const checkCity =  city == "NYC" ?  "Ok, sounds good." : "No go."
+  return checkCity;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case tip = 'generous':
+        return 'Thank you so much.';
+    case tip = 'not as generous':
+        return 'Thank you.';
+    break;
+    default: 
+      return "Bye."
+      break;
+  }
 }
